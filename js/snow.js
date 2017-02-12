2e40d70e683626b935abd3e640b59d3c8706112e
Douglas <theonlycooper@gmail.com>
2017-02-12 02:52:31 +0100

    refactoring

--- js/snow.js
+++ js/snow.js
@@ -22,14 +22,12 @@ function snow() {
 	}
 
 	//Lets draw the flakes
-	function draw()
-	{
+	function draw() {
 		ctx.clearRect(0, 0, W, H);
 
 		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
 		ctx.beginPath();
-		for(var i = 0; i < mp; i++)
-		{
+		for(var i = 0; i < mp; i++) {
 			var p = particles[i];
 			ctx.moveTo(p.x, p.y);
 			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
@@ -41,11 +39,9 @@ function snow() {
 	//Function to move the snowflakes
 	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
 	var angle = 0;
-	function update()
-	{
+	function update() {
 		angle += 0.01;
-		for(var i = 0; i < mp; i++)
-		{
+		for(var i = 0; i < mp; i++) {
 			var p = particles[i];
 			//Updating X and Y coordinates
 			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
@@ -56,22 +52,18 @@ function snow() {
 
 			//Sending flakes back from the top when it exits
 			//Lets make it a bit more organic and let flakes enter from the left and right also.
-			if(p.x > W+5 || p.x < -5 || p.y > H)
-			{
-				if(i%3 > 0) //66.67% of the flakes
-				{
+			if(p.x > W+5 || p.x < -5 || p.y > H) {
+				if(i%3 > 0) {//66.67% of the flakes {
 					particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
 				}
 				else
 				{
 					//If the flake is exitting from the right
-					if(Math.sin(angle) > 0)
-					{
+					if(Math.sin(angle) > 0) {
 						//Enter from the left
 						particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
 					}
-					else
-					{
+					else {
 						//Enter from the right
 						particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
 					}
