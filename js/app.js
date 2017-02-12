e3190e6f9890b7c5b0c0ccdcf6c6d4fcce3a8c4a
Douglas <theonlycooper@gmail.com>
2017-02-12 18:29:04 +0100

    Fade

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const weekColours = ["#193996", "#51173A", "#F8970C", "#8B1C62", "#175032",
                      "#d80a3a", "#820000", "#001063", "#af0000", "#000000",
                      "#87008c", "#115aad", "#a0ad11", "#00bfaf", "#e00000",
                      "#4f4f4f", "#3ca035", "#0e637c", "#ed0273", "#b7247f",
-                     "#4924b7", "#1f1882", "#188234", "#3d7dcc", "#074214",
+                     "#4924b7", "#1f1882", "#188234", "#3d7dcc", "#e89900",
                      "#470719", "#dd7c04", "#6904dd", "#8fbf00", "#00bf3c",
                      "#347887", "#270949", "#2f606d", "#382f6d", "#105e1b",
                      "#898989", "#037715", "#77035a", "#032077", "#bc185f",
@@ -15,18 +15,21 @@ function update() {
 
     if(newWeek!=thisWeek) {
         thisWeek=newWeek;
-        $('.week').html(thisWeek);
+
         autoColours();
-        if (thisWeek>47 || thisWeek<7) {
+        if (thisWeek>47 || thisWeek<3) {
             snow();
-            
         }
     }
 
 }
 
 function autoColours() {
-    $("body").css("background", weekColours[thisWeek-1]);
+    $("body").animate({backgroundColor: weekColours[thisWeek-1]}, 2000);
+
+    $(".week").fadeOut(1000, ()=> {
+      $(".week").html(thisWeek);
+    }).fadeIn(1000);
 }
 
 function getWeekNumber(d) {
